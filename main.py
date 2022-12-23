9ccb52a3f4f890b6a63b1ea060033dbd4bb05f21
Tverdohleb Egor <tverdegor@gmail.com>
2022-12-23 23:06:31 +0100

    Cleaned great part of the code

--- main.py
+++ main.py
@@ -4,99 +4,91 @@ from pynput import keyboard
 import threading
 import random
 
-DIMENSION = 50
+DIMENSION = 30
 #dimension of the gameplan
 STOPFLAG = False
 #flag usefull to stop the threads
 
+LOSESTRING = " Man you lose, that's prety disappointing \n\t  Press Enter to exit"
+WINSTRING = " Man you won, that's prety awesome \n\t  Press Enter to exit"
+
 snake_dimension = 1
-GameMatrix = [[" " for _ in range(DIMENSION)] for _ in range(DIMENSION//2 -1)]
+
 currentApple = None
 
-snake = [[DIMENSION//2-1, DIMENSION//4-1, 3]]
+snake = [[DIMENSION//2-1, DIMENSION//4-1, -1]]
 
 stringClearScreen = 'cls' if os.name == 'nt' else 'clear'
 
 def printMatrix(matrix):
     os.system(stringClearScreen)
+    
     print("-"*(DIMENSION+2))
+
     for line in matrix:
-        print("|",end="")
-        for ch in line:
-            print(ch,end="")
-        print("|")
+        print("|" + "".join(line) + "|")
+    
     print("-"*(DIMENSION+2))
+    
     time.sleep(0.2)
 
-def lose():
+def endGame(phrase):
     global STOPFLAG
+    
     os.system(stringClearScreen)
-    print("-"*50)
-    print("Man you lose, that's prety disappointing")
-    print("-"*50)
-    STOPFLAG = True
-
-def win():
-    global STOPFLAG
-    os.system(stringClearScreen)
-    print("-"*50)
-    print("Bro you won thats very cool")
-    print("-"*50)
+    
+    print("-"*42)
+    print(phrase)
+    print("-"*42)
+    
     STOPFLAG = True
 
 def moveSnake(snake,matrix):
     for indSnakePart in range(len(snake)):
         matrix[snake[indSnakePart][1]][snake[indSnakePart][0]] = " "
 
-        if snake[indSnakePart][2] == 0:
-            snake[indSnakePart][0] = (snake[indSnakePart][0]+1) % len(matrix[0])
-        elif snake[indSnakePart][2] == 2:
-            snake[indSnakePart][0] = (snake[indSnakePart][0]-1) % len(matrix[0])
-        elif snake[indSnakePart][2] == 1:
-            snake[indSnakePart][1] = (snake[indSnakePart][1]+1) % len(matrix)
-        elif snake[indSnakePart][2] == 3:
-            snake[indSnakePart][1] = (snake[indSnakePart][1]-1) % len(matrix)
+        addX = 1 if snake[indSnakePart][2] == 0 else -1 if snake[indSnakePart][2] == 2 else 0
+        addY = 1 if snake[indSnakePart][2] == 1 else -1 if snake[indSnakePart][2] == 3 else 0
+
+        snake[indSnakePart][0] = (snake[indSnakePart][0]+addX) % len(matrix[0])
+        snake[indSnakePart][1] = (snake[indSnakePart][1]+addY) % len(matrix)
     
         matrix[snake[indSnakePart][1]][snake[indSnakePart][0]] = "H" if indSnakePart == 0 else "#"
         
-        if(matrix[snake[0][1]][snake[0][0]] == "#"):
-            lose()
+        if((snake[0][0],snake[0][1]) in [(snake[s][0],snake[s][1]) for s in range(1,len(snake))] ):
+            endGame(LOSESTRING)
             return()
+
     for indSnakePart in range(len(snake)-1,0,-1):
         snake[indSnakePart][2] = snake[indSnakePart-1][2] 
 
 def spawnSnakeTail(matrix):
     global snake_dimension
-    newSnake = snake[-1][::1]
+    newSnake = snake[-1].copy()
 
-    if newSnake[2] == 0:
-        newSnake[0] = (newSnake[0]-1)% len(matrix[0])
-    elif newSnake[2] == 2:
-        newSnake[0] = (newSnake[0]+1) % len(matrix[0])
-    elif newSnake[2] == 1:
-        newSnake[1] = (newSnake[1]-1) % len(matrix)
-    elif newSnake[2] == 3:
-        newSnake[1] = (newSnake[1]+1) % len(matrix)
+    addX = -1 if newSnake[2] == 0 else 1 if newSnake[2] == 2 else 0
+    addY = -1 if newSnake[2] == 1 else 1 if newSnake[2] == 3 else 0
+
+    newSnake[0] = (newSnake[0]+addX) % len(matrix[0])
+    newSnake[1] = (newSnake[1]+addY) % len(matrix)
 
     snake_dimension+=1
+
     return newSnake
 
 def spawnApple(matrix):
     global currentApple
-    applePool = []
-    for x in range(len(matrix[0])):
-        for y in range(len(matrix)):
-            if(matrix[y][x] == " "):
-                applePool.append((x,y))
-    if len(applePool) == 0:
-        win()
-        return
-    currentApple = random.choice(applePool)
+    
+    possiblePositionForApple = [(x,y) for x in range(len(matrix[0])) for y in range(len(matrix)) if matrix[y][x] == " "]
+
+    currentApple = random.choice(possiblePositionForApple)
     matrix[currentApple[1]][currentApple[0]] = "o"
 
 
-def gameInput():
-    global snake
+def gameInput(snake):
+    
+    #TODO Change input system because this can have some flaws
+
     while not STOPFLAG:
         with keyboard.Events() as events:
             event = events.get(1e6)
@@ -113,27 +105,28 @@ def main():
     global currentApple
 
     os.system(stringClearScreen)
+    
     #x, y, direction
-    #Direction :
-    #0 right
-    #1 down
-    #2 left
-    #3 up
+    #Direction : 0 right 1 down 2 left 3 up
+
+    GameMatrix = [[" " for _ in range(DIMENSION)] for _ in range(DIMENSION//2 -1)]
     GameMatrix[snake[0][1]][snake[0][0]] = "H"
     
     spawnApple(GameMatrix)
 
     while not STOPFLAG:
         printMatrix(GameMatrix)
+        
         if(snake_dimension == (DIMENSION * ((DIMENSION//2)-1)) ):
-            win()
+            endGame(WINSTRING)
             return
+
         moveSnake(snake,GameMatrix)
-        #moveSnakeBody(snake,GameMatrix)
+        
         if((snake[0][0],snake[0][1]) == currentApple):
             snake.append(spawnSnakeTail(GameMatrix))
             spawnApple(GameMatrix)
 
 if __name__ == "__main__":
     threading.Thread(target=main).start()
-    threading.Thread(target=gameInput).start()
+    threading.Thread(target=gameInput(snake)).start()
